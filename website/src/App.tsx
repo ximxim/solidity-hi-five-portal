a981f3e740f6dac3dd7bfad26b9b2f998918f878
ximxim <40807667+lazerblasters@users.noreply.github.com>
2021-10-11 16:56:28 -0400

    personalized the page a little

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -17,11 +17,11 @@ export default function App() {
         </div>
 
         <div className="bio">
-        I am farza and I worked on self-driving cars so that's pretty cool right? Connect your Ethereum wallet and wave at me!
+        I am Azim and I worked on react native mobile apps so that's pretty cool right? Connect your Ethereum wallet and send a HiFive to me!
         </div>
 
-        <button className="waveButton" onClick={wave}>
-          Wave at Me
+        <button className="hiFiveButton" onClick={wave}>
+          Send me a HiFive
         </button>
       </div>
     </div>
